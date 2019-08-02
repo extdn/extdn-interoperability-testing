275d5f42cf7cfdb771344bd4baf2285d475ddda0
Jisse Reitsma <jisse@yireo.com>
2019-08-02 12:42:35 +0200

    Rename file and add better instructions

---
+++ scripts/example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Example: 
+#     composer config repositories.your-name composer https://your-api-to-your-composer-package
+#     composer require your/extension
+#     ./bin/magento module:enable Yireo_Extension
+#
+
--- scripts/your-composer-pre-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# Example: composer config repositories.your-name composer https://your-api-to-your-composer-package
-#
-
